dda194c1d3b735dd1031a5d9825f6e07622ac526
dannypellatt <danielpellatt92@gmail.com>
2022-07-01 12:10:58 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+
+string myName = "Danny";
+int myAge = 30;
+char firstLetter = 'a';
+bool isbFirstLetter = false;
+double oneHundredCents = 1.01;
+decimal fiftyCents = 0.50m;
+
+Console.WriteLine($"My name is {myName}, I am {myAge} years old");
+Console.WriteLine($"The first letter of the alphabet is: {firstLetter}, if I were to say it's b, that would be {isbFirstLetter}");
+Console.WriteLine($"One hundred and one cents is {oneHundredCents}, not {ß}");
